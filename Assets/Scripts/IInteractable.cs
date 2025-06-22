781cf8cea3c5bc12e646ef18d3cac85eea3c9cb6
zoeschwindt <schwindtzoe380@gmail.com>
2025-06-22 18:56:29 -0300

    IInteractable

--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
+//Interfaz IInteractable: define una estructura común que obliga a implementar el método Interact().
+// Se utiliza para aplicar polimorfismo: cualquier clase que implemente esta interfaz
 public interface IInteractable
 {
+    // Método que deben implementar todas las clases que quieran ser "interactuables".
     void Interact();
 }
